d251427617427ced661033541fbbcc89f3a41d65
lookman17 <otonkodok@gmail.com>
2025-04-16 10:32:01 +0700

    add form
    hanya menambahan form yang dibutuhkan

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace TPQ_DARUL_ULUM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
